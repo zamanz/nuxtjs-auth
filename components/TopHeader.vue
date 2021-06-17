f8d9c7f3653790e85cca3523af6a594e866a0e59
Zaman <zaman7u@gmail.com>
2021-06-17 14:13:41 +0600

    Update TopHeader.vue

--- components/TopHeader.vue
+++ components/TopHeader.vue
@@ -37,6 +37,16 @@
                                         </svg>
                                     </NuxtLink>
                                 </li>
+
+                                <li class="nav-item nav__item__rounded" title="Chat">
+                                    <NuxtLink to="/chat" class="nav-link d-flex align-items-center justify-content-center" aria-current="page">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left-dots" viewBox="0 0 16 16">
+                                            <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                                            <path d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                                        </svg>
+                                    </NuxtLink>
+                                </li>
+
                                 <li class="nav-item nav__item__rounded d-lg-none d-block" title="Search">
                                     <a class="nav-link d-flex align-items-center justify-content-center" href="#">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
@@ -119,12 +129,12 @@
                                 </li>
 
                                 <li class="nav-item" v-else>
-                                    <NuxtLink class="nav-link d-flex align-items-center justify-content-center" to="/login">
+                                    <NuxtLink class="nav-link d-flex align-items-center justify-content-center pe-0" to="/login">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
                                             <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
                                             <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
                                         </svg>
-                                        <span class="d-none d-lg-block">প্রবেশ করুন</span>
+                                        <span class="d-none d-lg-block ps-1">প্রবেশ করুন</span>
                                     </NuxtLink>
                                 </li>
                                 
